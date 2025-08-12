e28d0ac45929c099a280be6e9961c93b55d77ba5
Ayush Shakya <ayshakya24@gmail.com>
2025-08-12 13:03:01 +0530

    Wiring/Layout

--- frontend/src/pages/verification/Verification.jsx
+++
@@ -1,4 +0,0 @@
-export const Verification = () => {
-    return <h1>Verification</h1>
-}
-// Both login and register component will be rendered in this component, based of user's choice, this component will return the user data.
